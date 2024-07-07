c3fc9fff7793ab8b1257a24e7ebb085da27fac21
Vibhu0412 <vbpurohit1948@gmail.com>
2024-07-07 18:45:38 +0530

    remove blog, pricing and blog_details page

--- website/views.py
+++ website/views.py
@@ -17,16 +17,16 @@ def service(request):
     return render(request, 'website/service.html')
 
 
-def pricing(request):
-    return render(request, 'website/pricing.html')
-
-
-def blog(request):
-    return render(request, 'website/blog.html')
-
-
-def blog_details(request):
-    return render(request, 'website/blog_details.html')
+# def pricing(request):
+#     return render(request, 'website/pricing.html')
+#
+#
+# def blog(request):
+#     return render(request, 'website/blog.html')
+#
+#
+# def blog_details(request):
+#     return render(request, 'website/blog_details.html')
 
 
 def contact(request):
